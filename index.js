db595757683db87b8791ad3ab4f502b16042561b
OksanaBakl <oksanamolesha@ukr.net>
2021-08-06 21:58:29 +0300

    fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const INTERVAL = 1000;
+const INTERVAL_TIME = 1000;
 
 const startBtn = document.querySelector('button[data-action="start"]');
 const stopBtn = document.querySelector('button[data-action="stop"]');
@@ -22,16 +22,16 @@ startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click', onStopBtnClick);
 
 function onStartBtnClick() {
-  startBtn.removeAttribute('disable', '');
-  intervalId = setInterval(getBgColor, INTERVAL);
-}
-
-function getBgColor() {
-  document.body.style.background =
-    colors[randomIntegerFromInterval(0, colors.length - 1)];
+  startBtn.setAttribute('disable', '');
+  intervalId = setInterval(getBgColor, INTERVAL_TIME);
 }
 
 function onStopBtnClick() {
   startBtn.removeAttribute('disabled', '');
   clearInterval(intervalId);
+}
+
+function getBgColor() {
+  document.body.style.background =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
 }
